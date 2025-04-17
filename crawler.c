be06c3162f0fd4391b211fdb8d0d1e740a617b6a
Henry Kern <105322150+henryrifle@users.noreply.github.com>
2025-04-17 10:59:39 -0400

    Update crawler.c

--- crawler.c
+++ crawler.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <curl/curl.h>
+#include <string.h>
 
+#define MAX_URLS 100
+#define WORD_COUNT 3
+
+// Important words to count
+const char *important_words[WORD_COUNT] = {"Data", "Science", "Algorithm"};
